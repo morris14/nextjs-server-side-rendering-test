a410a1387de3e4d274f15e944d5656c049a07339
Stuart Morris <admin@handcode.co.uk>
2019-07-14 21:52:33 +0100

    Progress

--- pages/p/[id].js
+++ pages/p/[id].js
@@ -1,15 +1,22 @@
-import { useRouter } from "next/router";
 import Layout from "../../components/MyLayout";
+import fetch from "isomorphic-unfetch";
 
-const Post = () => {
-    const router = useRouter();
+const Post = ({ show }) => (
+    <Layout>
+        <h1>{show.name}</h1>
+        <p>{show.summary.replace(/<[/]?p>/g, "")}</p>
+        <img src={show.image.medium} />
+    </Layout>
+);
 
-    return (
-        <Layout>
-            <h1>{router.query.id}</h1>
-            <p>This is the blog post content.</p>
-        </Layout>
-    );
+Post.getInitialProps = async function(context) {
+    const { id } = context.query;
+    const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
+    const show = await res.json();
+
+    console.log(`Fetched show: ${show.name}`);
+
+    return { show };
 };
 
 export default Post;
